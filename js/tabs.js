b0659808ba97637b8fc393b3e2a27c2579765340
Syomin-ilua <71830796+Syomin-ilua@users.noreply.github.com>
2021-05-20 16:55:49 +0300

    Add files via upload

--- js/tabs.js
+++ js/tabs.js
@@ -1,23 +1,32 @@
-const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
-const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+const tabHandlerItems = document.querySelectorAll('[data-tabs-handler]');
+const tabFieldItems = document.querySelectorAll('[data-tabs-field]');
 const designTitles = document.querySelectorAll('.design__title');
 
-for (const tab of tabsHandlerElems) {
-	tab.addEventListener('click', () => {
-		tabsHandlerElems.forEach(item => {
-			if (tab === item) {
-				item.classList.add('design-list__item_active');
-			} else {
-				item.classList.remove('design-list__item_active');
-			}
-		})
 
-		tabsFieldElems.forEach(item => {
-			if(item.dataset.tabsField === tab.dataset.tabsHandler) {
-				item.classList.remove('hidden')
-			} else {
-				item.classList.add('hidden')
-			}
-		})
-	})
-}
+tabHandlerItems.forEach((handler, idx) => {
+    handler.addEventListener('click', () => {
+        for(const a of tabHandlerItems) {
+             if(handler === a) {
+                a.classList.add('design-list__item_active');
+             } else {
+                a.classList.remove('design-list__item_active');
+             }
+        }
+
+        for(const field of tabFieldItems) {
+            if (field.dataset.tabsField === handler.dataset.tabsHandler) {
+                field.classList.remove('hidden')
+            } else {
+                field.classList.add('hidden')
+            }
+        }
+
+        designTitles.forEach((designTitle, designTitleIdx) => {
+            if(idx === designTitleIdx) {
+                designTitle.classList.remove('hidden')
+            } else {
+                designTitle.classList.add('hidden')
+            }
+        })
+    })
+})
